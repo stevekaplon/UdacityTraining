5e7fe37511f9bbb929bd4f4c3d97f7602abb0756
steve.kaplon@omniresources.com <steve.kaplon@omniresources.com>
2017-10-27 09:56:42 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devServer: { inline: true },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /.*\.(png|svg|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+          template: 'src/index.html'
+        })
+      ]
+};
