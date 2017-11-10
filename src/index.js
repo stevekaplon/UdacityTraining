ea9e364ff0cec0ea5a6807a980002dcf102369ff
steve.kaplon@omniresources.com <steve.kaplon@omniresources.com>
2017-11-10 12:27:28 -0600

    cleanup

--- src/index.js
+++
@@ -1,7 +0,0 @@
-
-//require("./index.html");
-
-
-/*const image1 = require('./images/still_life.jpg');
-const image2 = require('responsive-loader!./images/still_life.jpg')
-console.log(JSON.stringify(image1));*/
